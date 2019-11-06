29e756aae16d5ada88087563577b6e47e7bb3d25
Tomasz Karbownicki <tomasz@karbownicki.com>
2019-11-06 19:16:12 +0100

    Basic componentes: Contact, About, Vod

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <TagenNav/>
     <router-view/>
+    <Footer/>
   </div>
 </template>
 
+<script>
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+
+import Footer from "@/components/Footer.vue";
+import TagenNav from "@/components/TagenNav.vue";
+
+UIkit.use(Icons);
+
+export default {
+  name: "home",
+  components: {
+    TagenNav,
+    Footer,
+  },
+};
+</script>
+
 <style lang="scss">
+@import "variables.scss";
+@import "../node_modules/uikit/src/scss/variables-theme.scss";
+@import "../node_modules/uikit/src/scss/mixins-theme.scss";
+@import "../node_modules/uikit/src/scss/uikit-theme.scss";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background: #FFF;
 }
 </style>
